a59b3efae65428a33463e70c4c8e4581d9214efd
Panibrat <a.panibratenko@gmail.com>
2017-07-04 14:57:51 +0300

    Vanila JS chat ready

--- index.js
+++ index.js
@@ -5,9 +5,21 @@ var app = express();
 var server  = app.listen(5555);
 var staticDir = __dirname + "/public/";
 
+var messages = [{name: '1st name', data: '1st msg'}];
+
 app.use(bodyPareser.json());
-app.use(bodyPareser.urlencoded());
+app.use(bodyPareser.urlencoded({extended: false}));
 
 app.get('/', function (req, res) {
-    res.sendfile(staticDir + 'index.html');
-})
+    res.sendFile(staticDir + 'index.html');
+});
+
+app.get('/messages', function(req, res){
+    res.json(messages);
+});
+
+app.post('/messages', function(req, res){
+    var message = req.body;
+    messages.push(message);
+    res.json(messages);
+});
