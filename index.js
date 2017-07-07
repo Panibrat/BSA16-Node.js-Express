1f4fa2e07f7710f72a74d904d9901c1ea197f291
Panibrat <a.panibratenko@gmail.com>
2017-07-07 14:41:51 +0300

    init socket io

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 var express = require("express");
 var bodyPareser = require("body-parser");
+var socketio = require("socket.io");
 
 var app = express();
 var server  = app.listen(5555);
+var io = socketio.listen(server);
+
 var staticDir = __dirname + "/public/";
 
 var messages = [{name: '1st name', data: '1st msg'}];
@@ -22,4 +25,20 @@ app.post('/messages', function(req, res){
     var message = req.body;
     messages.push(message);
     res.json(messages);
+});
+
+app.get('/sockets', function(req, res){
+    res.sendFile(staticDir + 'index.html');
+});
+
+io.on('connection', function (socket) {
+    console.log("Client Connected ");
+    socket.on('disconnected', function(){
+        console.log("Client Disconnected ");
+    });
+    socket.on('chat message', function (msg) {
+        messages.push(msg);
+        io.emit('chat message', msg);
+    });
+    socket.emit('chat history', messages);
 });
